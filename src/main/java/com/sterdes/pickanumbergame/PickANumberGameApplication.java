c5196da6690c4519354e8b4ec622d58843eab247
kuldarvakker <kuvakk@taltech.ee>
2023-10-16 19:21:23 +0300

    initial

---
+++ src/main/java/com/sterdes/pickanumbergame/PickANumberGameApplication.java
@@ -0,0 +1,13 @@
+package com.sterdes.pickanumbergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickANumberGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickANumberGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sterdes/pickanumbergame/PickANumberGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sterdes.pickanumbergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickANumberGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
